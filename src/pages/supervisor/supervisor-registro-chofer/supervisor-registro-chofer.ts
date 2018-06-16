a1108819c8ebc0980626eaca8ab3a949f423b178
daniel9898 <danyillusion@yahoo.com.ar>
2018-06-16 20:21:02 -0300

    readme modificado

--- src/pages/supervisor/supervisor-registro-chofer/supervisor-registro-chofer.ts
+++ src/pages/supervisor/supervisor-registro-chofer/supervisor-registro-chofer.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'; 
 
 @IonicPage()
 @Component({
@@ -8,11 +9,32 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SupervisorRegistroChoferPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  rForm: FormGroup;
+ 
+  constructor(public navCtrl: NavController, public navParams: NavParams, private fb: FormBuilder) {
+  	this.setFormValidator();
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SupervisorRegistroChoferPage');
+  /*
+	logeo
+	boton - inicio actividad
+	asignacion de vehiculo
+	encuesta con el qr
+	lista de espera
+
+  */
+  
+  setFormValidator(){
+    this.rForm = this.fb.group({
+      'nombre' : ['',  Validators.compose([Validators.required, Validators.minLength(4), Validators.maxLength(10)])],
+      'apellido' : ['', Validators.compose([Validators.required, Validators.minLength(4), Validators.maxLength(10)])],
+      'edad' : ['', Validators.pattern('/[0-9\+\-\ ]/')],
+      'direccion' : ['', Validators.compose([Validators.required, Validators.minLength(4), Validators.maxLength(15)])],
+      'perfil' : ['chofer'],
+      'email' : ['', Validators.email],
+      'imagen' : [''],
+      'viajando' : [false]
+    });
   }
 
 }
