44461041f6028fd562e0fbc3926f5210c8c19b3a
EzequielCaballero <ezequiel.caballero33@gmail.com>
2018-06-18 23:44:28 -0300

    Cliente inicio - pedir viaje / Cliente viaje - punto origen + destino

--- src/pages/cliente/cliente-viaje/cliente-viaje.ts
+++ src/pages/cliente/cliente-viaje/cliente-viaje.ts
@@ -1,25 +1,73 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
+//PAGINAS
+import { MapaPage } from '../../index-paginas';
+//clase USUARIO
+import { Usuario } from '../../../classes/usuario';
+//SERVICIOS
+import { UsuarioServicioProvider } from '../../../providers/usuario-servicio/usuario-servicio';
+import { AuthServicioProvider } from '../../../providers/auth-servicio/auth-servicio';
 
-/**
- * Generated class for the ClienteViajePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
-@IonicPage()
 @Component({
   selector: 'page-cliente-viaje',
   templateUrl: 'cliente-viaje.html',
 })
 export class ClienteViajePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  usuario:Usuario;
+  mostrarSpinner:boolean = false;
+  origen:string;
+  destino:string;
+  punto:number; // 1-Origen / 2-Destino
+  //CALLBACK function (para retornar dirección desde MapaPage)
+  myCallbackFunction:Function;
+
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              private _auth:AuthServicioProvider,
+              private _userService:UsuarioServicioProvider) {
+
+      this.mostrarSpinner = true;
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad ClienteViajePage');
+
+    this.traer_usuario();
+
+    //callBack para mapa
+    this.myCallbackFunction = (_params)=> {
+      console.log("callback asignado");
+       return new Promise((resolve, reject) => {
+            if(this.punto == 1)
+                this.origen = _params;
+            if(this.punto == 2)
+                this.destino = _params;
+                resolve();
+           });
+    }
+  }
+
+  //TRAER UN USUARIO
+  traer_usuario(){
+    this.mostrarSpinner = true;
+    this._userService.traer_un_usuario(this._auth.get_userUID())
+      .then((user:any)=>{
+        console.log("Usuario: " + JSON.stringify(user));
+        this.usuario = user;
+        this.origen = user.direccion;
+        this.mostrarSpinner = false;
+      })
+      .catch((error)=>{
+        this.mostrarSpinner = false;
+        console.log("Error al traer usuario: " + error);
+      })
+  }
+
+  //MOSTRAR MAPA
+  verMapa(opcion:number){
+    this.punto = opcion;
+    this.navCtrl.push(MapaPage, {'direccion' : this.usuario.direccion, 'callback':this.myCallbackFunction});
   }
 
 }
