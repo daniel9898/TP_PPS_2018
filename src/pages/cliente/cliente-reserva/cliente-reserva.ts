e51dfceeeec3c1e1b0ebc61b7185cb5dac8c00ee
Lucas Bonanni <lucas_eb2004@yahoo.com.ar>
2018-06-21 22:02:21 -0300

    se modifica la pagina cliente reserva

--- src/pages/cliente/cliente-reserva/cliente-reserva.ts
+++ src/pages/cliente/cliente-reserva/cliente-reserva.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage} from 'ionic-angular';
-
+import { IonicPage } from 'ionic-angular';
+import { DateTimeProvider } from '../../../providers/date-time/date-time';
 
 @IonicPage()
 @Component({
@@ -8,11 +8,24 @@ import { IonicPage} from 'ionic-angular';
   templateUrl: 'cliente-reserva.html',
 })
 export class ClienteReservaPage {
-  
 
 
-  constructor() {}
+  public fecha: Date;
+  public hora: Date;
+  monthNames: string[];
+  monthShortNames: string[];
+  daysNames: string[];
+  daysShortNames: string[];
 
- 
+  constructor(dateTimeSrv: DateTimeProvider) {
+    this.hora = new Date();
+    this.hora = new Date(this.hora.toLocaleTimeString());
+    this.monthNames = dateTimeSrv.getMonthNames();
+    this.daysNames = dateTimeSrv.getWeekDays();
+    this.daysShortNames = dateTimeSrv.getWeekDaysShort();
+    this.monthShortNames = dateTimeSrv.getMonthNamesShort();
+  }
+
+
 
 }
