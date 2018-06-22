53e9e6f8538f0714cc206acfe0d8ec92fef46a70
Lucas Bonanni <lucas_eb2004@yahoo.com.ar>
2018-06-22 09:44:57 -0300

    se hace funcionar el callback y la llamada del mapa

--- src/pages/cliente/cliente-reserva/cliente-reserva.ts
+++ src/pages/cliente/cliente-reserva/cliente-reserva.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { DateTimeProvider } from '../../../providers/date-time/date-time';
+import { MapaPage } from '../../index-paginas';
 
 @IonicPage()
 @Component({
@@ -9,15 +10,21 @@ import { DateTimeProvider } from '../../../providers/date-time/date-time';
 })
 export class ClienteReservaPage {
 
-
   public fecha: Date;
   public hora: Date;
   monthNames: string[];
   monthShortNames: string[];
   daysNames: string[];
   daysShortNames: string[];
+  public direccionDest: string = '';
+  public direccionOrigin: string = '';
+  myOriginCallbackFunction: Function;
+  myDestCallbackFunction: Function;
 
-  constructor(dateTimeSrv: DateTimeProvider) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    dateTimeSrv: DateTimeProvider) {
     this.hora = new Date();
     this.hora = new Date(this.hora.toLocaleTimeString());
     this.monthNames = dateTimeSrv.getMonthNames();
@@ -26,6 +33,40 @@ export class ClienteReservaPage {
     this.monthShortNames = dateTimeSrv.getMonthNamesShort();
   }
 
+  ionViewDidLoad() {
+    this.myOriginCallbackFunction = (_params) => {
+      console.log("callback asignado");
+      return new Promise((resolve, reject) => {
+        this.direccionOrigin = _params;
+        resolve();
+      });
+    }
+    this.myDestCallbackFunction = (_params) => {
+      console.log("callback asignado");
+      return new Promise((resolve, reject) => {
+        this.direccionDest = _params;
+        resolve();
+      });
+    }
+  }
 
+
+  public setDir(dir) {
+    this.direccionOrigin = dir;
+  }
+
+  /**
+   * Setea la dirección de origen
+   */
+  setOriginDir() {
+    this.navCtrl.push(MapaPage, { 'direccion': this.direccionOrigin, 'callback': this.myOriginCallbackFunction });
+  }
+
+  /**
+   * Setea la dirección de destino
+   */
+  setDestDir() {
+    this.navCtrl.push(MapaPage, { 'direccion': this.direccionDest, 'callback': this.myDestCallbackFunction });
+  }
 
 }
