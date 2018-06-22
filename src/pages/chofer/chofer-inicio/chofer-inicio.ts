656a105c2cf7cebccae47dc3c3496c0c061ebec2
daniel9898 <danyillusion@yahoo.com.ar>
2018-06-22 18:11:57 -0300

    asignacion de vehiculo

--- src/pages/chofer/chofer-inicio/chofer-inicio.ts
+++ src/pages/chofer/chofer-inicio/chofer-inicio.ts
@@ -15,34 +15,38 @@ export class ChoferInicioPage {
   usuarioSesion:any;
   asignado: boolean = false;
   vehiculos: any;
+  vehiculoAsignado : any;
 
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
               public utils: UtilidadesProvider,
               public vehiculosProv :VehiculosProvider) {
+        
+    this.usuarioSesion = firebase.auth().currentUser;
 
-        this.usuarioSesion = firebase.auth().currentUser;
-        console.log("Usuario actual: " + JSON.stringify(this.usuarioSesion));
-
-        this.vehiculosProv.getListaVehiculos().subscribe(
-          lista => this.vehiculos = lista,
-          error => console.log(error)
-        )
+    this.vehiculosProv.getListaVehiculos().subscribe(
+      lista => this.vehiculos = lista,
+      error => this.utils.showAlert('Atención !',error.json())
+    )
   }
 
-    /*
-  logeo
-  boton - inicio actividad
-  asignacion de vehiculo
-  encuesta con el qr
-  lista de espera
-
-  */
-
-  comenzar(){
-
+  comenzarActividad(){
+    this.utils.showLoading();
     console.log('vehiculos',this.vehiculos);
+    
+    for(let i=0; i<this.vehiculos.length; i++){
+        if(this.vehiculos[i].vehiculo.activo && !this.vehiculos[i].vehiculo.ocupado){
+           this.vehiculoAsignado = this.vehiculos[i];
+           this.asignado = true;
+           break;
+        }
+    }
 
+    //FALTA GUARDAR EL VEHICULO ASIGNADO
+
+    if(!this.asignado) { this.utils.showAlert('Atención !','Por el momento no hay vehiculos disponibles reintente más tarde.') }
+
+    this.utils.dismissLoading();
   }
 
 }
