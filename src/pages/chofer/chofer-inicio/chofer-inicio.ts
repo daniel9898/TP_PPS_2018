2223fec8629bce6758f52de539146edfd3f17aae
daniel9898 <danyillusion@yahoo.com.ar>
2018-06-23 17:45:50 -0300

    funcionalidad en pagina chofer inicio

--- src/pages/chofer/chofer-inicio/chofer-inicio.ts
+++ src/pages/chofer/chofer-inicio/chofer-inicio.ts
@@ -4,6 +4,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import * as firebase from 'firebase/app';
 import { UtilidadesProvider } from '../../../providers/utilidades/utilidades';
 import { VehiculosProvider } from '../../../providers/vehiculos/vehiculos';
+import { UsuarioServicioProvider } from '../../../providers/usuario-servicio/usuario-servicio';
+import { ListaViajesPage } from '../../index-paginas'; 
 
 @IonicPage()
 @Component({
@@ -16,11 +18,13 @@ export class ChoferInicioPage {
   asignado: boolean = false;
   vehiculos: any;
   vehiculoAsignado : any;
+  userUpdate:any;
 
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
               public utils: UtilidadesProvider,
-              public vehiculosProv :VehiculosProvider) {
+              public vehiculosProv :VehiculosProvider,
+              public userProv: UsuarioServicioProvider) {
         
     this.usuarioSesion = firebase.auth().currentUser;
 
@@ -31,22 +35,39 @@ export class ChoferInicioPage {
   }
 
   comenzarActividad(){
+    
     this.utils.showLoading();
     console.log('vehiculos',this.vehiculos);
-    
-    for(let i=0; i<this.vehiculos.length; i++){
-        if(this.vehiculos[i].vehiculo.activo && !this.vehiculos[i].vehiculo.ocupado){
-           this.vehiculoAsignado = this.vehiculos[i];
-           this.asignado = true;
-           break;
-        }
-    }
 
-    //FALTA GUARDAR EL VEHICULO ASIGNADO
+    try{
+        this.vehiculos.map(v => {
+          if(v.vehiculo.activo && !v.vehiculo.ocupado){
+             this.vehiculoAsignado = v;
+             this.asignado = true;
+             throw 'break';
+          }
+        })
+
+    }catch(e){
+      console.log(e);
+    }
+ 
 
     if(!this.asignado) { this.utils.showAlert('Atención !','Por el momento no hay vehiculos disponibles reintente más tarde.') }
 
+    this.guardarVehiculo();
     this.utils.dismissLoading();
   }
+
+  guardarVehiculo(){
+    this.userProv.asignarVehiculo(this.usuarioSesion.uid,this.vehiculoAsignado.key);
+
+  }
+
+  listadoDeViajes(){
+    this.navCtrl.setRoot(ListaViajesPage);
+  }
+
+
 
 }
