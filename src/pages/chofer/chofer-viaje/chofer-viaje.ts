4a68cdc6fbea38e0f6e8744ed11562c602250a7b
daniel9898 <danyillusion@yahoo.com.ar>
2018-06-23 17:48:22 -0300

    funcionalidad en pagina chofer viaje

--- src/pages/chofer/chofer-viaje/chofer-viaje.ts
+++ src/pages/chofer/chofer-viaje/chofer-viaje.ts
@@ -1,12 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the ChoferViajePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { UsuarioServicioProvider } from '../../../providers/usuario-servicio/usuario-servicio';
+import { Viaje } from '../../../classes/viaje';
+import { ViajeServicio } from '../../../providers/viaje-servicio/viaje-servicio';
 
 @IonicPage()
 @Component({
@@ -15,11 +11,39 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ChoferViajePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  chofer : any;
+  cliente : any;
+  viaje: Viaje;
+
+  constructor(public navCtrl: NavController, 
+  	          public navParams: NavParams,
+  	          public userProv: UsuarioServicioProvider,
+  	          public viajesProv : ViajeServicio) {
+    
+  	this.chofer = this.navParams.get('chofer');
+    this.viaje =  new Viaje(this.navParams.get('viaje'));
+
+    console.log('CHOFER : ',this.chofer);
+    console.log('VIAJE : ',this.viaje);
+   
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ChoferViajePage');
+  ionViewDidLoad(){
+    this.traerCliente();
   }
+
+  traerCliente(){ //VER EN QUE MOMENTO SE EJECUTA,ROMPE LA VISTA
+  
+    this.userProv.traerUsuario(this.viaje.id_cliente)
+                     .then(cliente => this.cliente = cliente)
+                     .catch(e => console.log(e.message));
+  }
+
+  modificarEstado(estado: string){
+    this.viaje.estado = estado;
+    this.viajesProv.modificar_viaje(this.viaje);
+  }
+
+ 
 
 }
