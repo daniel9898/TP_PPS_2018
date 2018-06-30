2aa1f11e3eb275ce4cdc5ac0afdccb5370aeb00a
daniel9898 <danyillusion@yahoo.com.ar>
2018-06-30 02:02:54 -0300

    se cambio el registro de chofer utilizando servicio auth admin

--- src/pages/supervisor/supervisor-registro-chofer/supervisor-registro-chofer.ts
+++ src/pages/supervisor/supervisor-registro-chofer/supervisor-registro-chofer.ts
@@ -4,6 +4,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 //PAGINAS
 import { SupervisorInicioPage,SupervisorListaChoferesPage } from '../../index-paginas';
 import { ChoferProvider } from '../../../providers/chofer/chofer';
+import { AuthAdministradorProvider } from '../../../providers/auth-administrador/auth-administrador';
 import { UsuarioImagenProvider } from '../../../providers/usuario-imagen/usuario-imagen';
 import { UtilidadesProvider } from '../../../providers/utilidades/utilidades';
 import { Camera } from '@ionic-native/camera';
@@ -28,19 +29,11 @@ export class SupervisorRegistroChoferPage {
               private chofer: ChoferProvider,
               public camera: Camera,
               public usrFoto: UsuarioImagenProvider,
-              public utils: UtilidadesProvider) {
+              public utils: UtilidadesProvider,
+              public _authAdmin: AuthAdministradorProvider) {
 
   	this.setFormValidator();
   }
-
-  /*
-	logeo
-	boton - inicio actividad
-	asignacion de vehiculo
-	encuesta con el qr
-	lista de espera
-
-  */
 
   setFormValidator(){
     this.rForm = this.fb.group({
@@ -67,8 +60,10 @@ export class SupervisorRegistroChoferPage {
     try{
 
         let credenciales = { email: this.rForm.value.correo, password: this.rForm.value.clave};
-     	  let authOk = await this.chofer.altaAuth(credenciales);
-
+     	  let authOk = await this._authAdmin.signUpExterno(credenciales);
+        await this._authAdmin.update_externalUserAccount(authOk.user,this.rForm.value.perfil,'http://www.radiozero.cl/static/2018/06/capturadepantalla20180626alas22627pm-bb432d2169693c2c90256fe777dded74-1200x600-1200x500.jpg');
+        await this._authAdmin.signOutExternal();
+        
         let chofer =  this.rForm.value;
 
         this.image != null ?  chofer.foto = await this.usrFoto.subirImagenUsuario(authOk.user.uid,this.image) :
