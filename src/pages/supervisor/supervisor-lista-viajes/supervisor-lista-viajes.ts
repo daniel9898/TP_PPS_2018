fd3c1455b8fa10b6c42b8009c54008041a070136
Lucas Bonanni <lucas_eb2004@yahoo.com.ar>
2018-06-30 02:39:55 -0300

    se muestra la lista de viajes en pantalla de viajes del supervisor

--- src/pages/supervisor/supervisor-lista-viajes/supervisor-lista-viajes.ts
+++ src/pages/supervisor/supervisor-lista-viajes/supervisor-lista-viajes.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ViajeServicio } from '../../../providers/viaje-servicio/viaje-servicio';
 
 /**
  * Generated class for the SupervisorListaViajesPage page.
@@ -15,11 +16,19 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SupervisorListaViajesPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  viajes: Array<any>;
+
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    private viajeSrv:ViajeServicio) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SupervisorListaViajesPage');
+    this.viajeSrv.getAllTrips().subscribe(next => {
+      this.viajes = next.filter(viaje => viaje.estado === 'pendiente');
+    });
   }
 
 }
