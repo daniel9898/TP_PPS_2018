3cdb654c04c3f8ad21451eafe1ea92b290205568
Lucas Bonanni <lucas_eb2004@yahoo.com.ar>
2018-07-01 20:04:48 -0300

    se agrega lógica a la lista de reservas

--- src/pages/supervisor/supervisor-lista-reservas/supervisor-lista-reservas.ts
+++ src/pages/supervisor/supervisor-lista-reservas/supervisor-lista-reservas.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ReservasProvider } from '../../../providers/reservas/reservas';
+import { SupervisorChoferesDisponiblesPage } from '../supervisor-choferes-disponibles/supervisor-choferes-disponibles';
 
 /**
  * Generated class for the SupervisorListaReservasPage page.
@@ -14,12 +16,40 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'supervisor-lista-reservas.html',
 })
 export class SupervisorListaReservasPage {
+  reservas: any[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private reservasSrv: ReservasProvider) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SupervisorListaReservasPage');
+    this.reservasSrv.getListaReservas().subscribe(next => {
+      this.reservas = next.filter(reserva => reserva.reserva.estado === 'pendiente');
+      console.log(next);
+    });
+  }
+
+  /**
+ * push a la lista de choferes
+ * @param index indice del array de viajes
+ */
+  verChoferes(index) {
+    const viaje = this.reservas[index];
+    this.navCtrl.push(SupervisorChoferesDisponiblesPage, { viaje: viaje, esReserva: true });
+  }
+
+
+  /**
+   * Cancela la reserva
+   * @param index indice del viaje selecionado
+   */
+  cancelarReserva(index) {
+    const item = this.reservas[index]
+    item.reserva.estado = 'cancelado';
+    this.reservasSrv.updateItem(item.key, item.reserva);
   }
 
 }
