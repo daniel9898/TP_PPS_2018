7112ea7e8ec2dd8b41220453269fc5c31fa30147
Lucas Bonanni <lucas_eb2004@yahoo.com.ar>
2018-07-01 17:17:24 -0300

    Agrego un gráfico de barras para el supervisor con los viajes por estado

--- src/pages/supervisor/supervisor-inicio/supervisor-inicio.ts
+++ src/pages/supervisor/supervisor-inicio/supervisor-inicio.ts
@@ -34,15 +34,24 @@ export class SupervisorInicioPage {
     responsive: true,
     maintainAspectRatio: false
   };
+  // END DONUT CHART
+  // START BAR CHART
+  public barChartOptions: any = {
+    legend: {
+      display: false,
+    },
+    scaleShowVerticalLines: false,
+    responsive: true,
+    maintainAspectRatio: false
+  };
+  public barChartLabels: string[] = ['pendiente', 'cancelado', 'en curso', 'cumplido'];
+  public barChartData: any[] = [
+    // {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
+    // {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
+  ];
+  public barChartType: string = 'bar';
+  public barChartLegend: boolean = true;
 
-  // events
-  public chartClicked(e: any): void {
-    console.log(e);
-  }
-
-  public chartHovered(e: any): void {
-    console.log(e);
-  }
   constructor(public navCtrl: NavController,
     public navParams: NavParams,
     public _auth: AuthServicioProvider,
@@ -52,6 +61,18 @@ export class SupervisorInicioPage {
     this.user_perfil = this._auth.get_userProfile();
     this.user_photo = this._auth.get_userPhoto();
     console.log("Usuario actual: " + this.user_perfil);
+    this.viajesSrv.getAllTrips().subscribe(next => {
+      const pendientes = next.filter(viaje => viaje.estado === 'pendiente').length;
+      const cancelados = next.filter(viaje => viaje.estado === 'cancelado').length;
+      const enCurso = next.filter(viaje => viaje.estado === 'en curso').length;
+      const cumplido = next.filter(viaje => viaje.estado === 'cumplido').length;
+      this.barChartData = [
+        { x: 'pendientes', y: pendientes },
+        { x: 'cancelados', y: cancelados },
+        { x: 'En curso', y: enCurso },
+        { x: 'cumplidos', y: cumplido }
+      ];
+    });
   }
 
   ionViewDidLoad() {
@@ -61,6 +82,18 @@ export class SupervisorInicioPage {
       const v = next.filter(usr => usr.perfil == 'chofer' && usr.activo).length;
       this.doughnutChartData = [disponibles, v]
     });
+
+  }
+
+
+
+  // events
+  public chartClicked(e: any): void {
+    console.log(e);
+  }
+
+  public chartHovered(e: any): void {
+    console.log(e);
   }
 
 }
