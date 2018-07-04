ac72dd0bf8a88bcc43baf7eaa814ad8b0887be78
daniel9898 <danyillusion@yahoo.com.ar>
2018-07-03 22:48:00 -0300

    se agrego imagen para inicio chofer y un modal para mostrar datos del viaje cuando finaliza

---
+++ src/pages/modal/modal.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams,ModalController, Platform, ViewController } from 'ionic-angular';
+
+/**
+ * Generated class for the ModalPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-modal',
+  templateUrl: 'modal.html',
+})
+export class ModalPage {
+
+ viaje :any;
+
+  constructor(
+    public platform: Platform,
+    public nav_params: NavParams,
+    public viewCtrl: ViewController
+  ) {
+
+
+    this.viaje = this.nav_params.get('viaje');
+    console.log('viaje en modal ',this.viaje);
+  }
+
+  dismiss() {
+    this.viewCtrl.dismiss();
+  }
+
+}
