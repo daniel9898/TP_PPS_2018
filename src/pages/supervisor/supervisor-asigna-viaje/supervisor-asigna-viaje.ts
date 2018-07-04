58f214b254dc92663140df15fbd57924602892fd
Lucas Bonanni <lucas_eb2004@yahoo.com.ar>
2018-07-04 18:31:02 -0300

    se agrega la asignación de reservas

--- src/pages/supervisor/supervisor-asigna-viaje/supervisor-asigna-viaje.ts
+++ src/pages/supervisor/supervisor-asigna-viaje/supervisor-asigna-viaje.ts
@@ -5,6 +5,8 @@ import { ViajeServicio } from '../../../providers/viaje-servicio/viaje-servicio'
 import { Viaje } from '../../../classes/viaje';
 import { SupervisorListaViajesPage } from '../supervisor-lista-viajes/supervisor-lista-viajes';
 import { Usuario } from '../../../classes/usuario';
+import { ReservasProvider } from '../../../providers/reservas/reservas';
+import { Reserva } from '../../../classes/viaje.model';
 
 /**
  * Generated class for the SupervisorAsignaViajePage page.
@@ -21,16 +23,28 @@ import { Usuario } from '../../../classes/usuario';
 export class SupervisorAsignaViajePage {
   chofer: Usuario;
   viaje: Viaje;
+  reserva: Reserva
+  esReserva: boolean = false;
+  key: string;
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     public usuarioSrv: UsuarioServicioProvider,
-    public viajeSrv: ViajeServicio) {
-    if (this.navParams.data.viaje && this.navParams.data.chofer) {
-      this.viaje = this.navParams.data.viaje;
-      this.chofer = this.navParams.data.chofer;
-      console.log(this.viaje, this.chofer);
+    public viajeSrv: ViajeServicio,
+    private reservaSrv: ReservasProvider) {
+    if (!this.navParams.data.esReserva) {
+      if (this.navParams.data.viaje && this.navParams.data.chofer) {
+        this.viaje = this.navParams.data.viaje;
+        this.chofer = this.navParams.data.chofer;
+        this.esReserva = this.navParams.data.esReserva;
+        console.log(this.viaje, this.chofer);
+      }
+    } else if (this.navParams.data.esReserva) {
+      this.reserva = this.navParams.data.viaje.reserva;
+      this.key = this.navParams.data.viaje.key;
+      this.esReserva = this.navParams.data.esReserva;
     }
+
   }
 
   ionViewDidLoad() {
@@ -43,11 +57,16 @@ export class SupervisorAsignaViajePage {
       .then(result => {
         console.log(result, 'ok')
         this.viaje.id_chofer = this.chofer.key;
-        this.viajeSrv.modificar_viaje(this.viaje)
-          .then(viaje => {
-            console.log(viaje, 'ok');
-            this.navCtrl.popTo(SupervisorListaViajesPage);
-          });
+        if (!this.esReserva) {
+          this.viajeSrv.modificar_viaje(this.viaje)
+            .then(viaje => {
+              console.log(viaje, 'ok');
+              this.navCtrl.popTo(SupervisorListaViajesPage);
+            });
+        } else if (this.esReserva) {
+          this.reservaSrv.updateItem(this.key,this.reserva);
+        }
+
       });
   }
 
