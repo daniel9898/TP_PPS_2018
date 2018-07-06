008e8230a822357bebeed9a6fba8c5535f15c56c
Lucas Bonanni <lucas_eb2004@yahoo.com.ar>
2018-07-06 00:16:53 -0300

    fixes varios en los módulos de Daniel Pereira

--- src/pages/chofer/chofer-viaje/chofer-viaje.ts
+++ src/pages/chofer/chofer-viaje/chofer-viaje.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavParams,ModalController } from 'ionic-angular';
+import { IonicPage, NavParams, ModalController } from 'ionic-angular';
 import { UsuarioServicioProvider } from '../../../providers/usuario-servicio/usuario-servicio';
 import { ViajeServicio } from '../../../providers/viaje-servicio/viaje-servicio';
 //import { Subscription } from 'rxjs/Subscription';
@@ -13,53 +13,53 @@ import { UtilidadesProvider } from '../../../providers/utilidades/utilidades';
 export class ChoferViajePage {
 
   //chofer : any;
-  cliente : any;
+  cliente: any;
   viaje: any;
-  
-  constructor( 
-  	          public navParams: NavParams,
-  	          public userProv: UsuarioServicioProvider,
-  	          public viajesProv : ViajeServicio,
-              public utils : UtilidadesProvider,
-              public modalCtrl: ModalController) {
+
+  constructor(
+    public navParams: NavParams,
+    public userProv: UsuarioServicioProvider,
+    public viajesProv: ViajeServicio,
+    public utils: UtilidadesProvider,
+    public modalCtrl: ModalController) {
 
 
     //this.chofer = this.navParams.get('chofer');
     this.viaje = this.navParams.get('viaje');
-  
+
     //console.log('CHOFER : ',this.chofer);
-    
-   
+
+
   }
 
-  ionViewDidLoad(){
+  ionViewDidLoad() {
 
     this.traerCliente();
-    
+
   }
 
-  traerCliente(){ //VER EN QUE MOMENTO SE EJECUTA,ROMPE LA VISTA
-  
+  traerCliente() { //VER EN QUE MOMENTO SE EJECUTA,ROMPE LA VISTA
+
     this.userProv.traerUsuario(this.viaje.id_cliente)
-                     .then(c => this.cliente = c)
-                     .catch(e => console.log(e.message));
+      .then(c => this.cliente = c)
+      .catch(e => console.log(e.message));
   }
 
-  async modificarEstado(estado: string){
+  async modificarEstado(estado: string) {
     this.viaje.estado = estado;
     await this.viajesProv.modificar_viaje(this.viaje);
-    console.log('viaje ',this.viaje);
-    if(estado != 'cumplido'){
-      this.utils.showToast('Viaje en Estado : '+estado,'success');
-    }else{
-      this.showModal();
+    console.log('viaje ', this.viaje);
+    if (estado != 'cumplido') {
+      this.utils.showToast('Viaje en Estado : ' + estado);
+    } else {
+      // this.showModal();
     }
   }
-  
+
   showModal() {
-   let contactModal = this.modalCtrl.create('ModalPage',{viaje:this.viaje});
-   contactModal.present();
- }
- 
+    let contactModal = this.modalCtrl.create('ModalPage', { viaje: this.viaje });
+    contactModal.present();
+  }
+
 
 }
