bd3972467b67024ed16fb3ed0df531a5dcf944fb
EzequielCaballero <ezequiel.caballero33@gmail.com>
2018-07-06 15:14:37 -0300

    Validación sonido on/off según atributo usuario en DB (inicio)

--- src/providers/sonidos/sonidos.ts
+++ src/providers/sonidos/sonidos.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@angular/core';
+//FIREBASE
+import { AngularFireAuth } from 'angularfire2/auth';
+//SERVICIOS
+import { UsuarioServicioProvider } from '../usuario-servicio/usuario-servicio';
 
 @Injectable()
 export class SonidosProvider {
@@ -12,8 +16,36 @@ export class SonidosProvider {
   success_sound:string = "assets/sounds/success_sound.mp3";
   car_engine:string = "assets/sounds/car_start_engine.mp3";
 
-  constructor() {
+  constructor(private _usuarioServicio:UsuarioServicioProvider,
+              private afAuth:AngularFireAuth) {
+
     console.log('Sonidos servicio');
+    //this.habilitado = true;
+    this.initialize();
+  }
+
+  initialize(){
+    let promesa = new Promise((resolve, reject)=>{
+        if(this.afAuth.auth.currentUser !== null){
+          console.log("Usuario logueado: " + this.afAuth.auth.currentUser.uid);
+          this._usuarioServicio.traerUsuario(this.afAuth.auth.currentUser.uid)
+            .then((user:any)=>{
+              if(user){
+                  this.habilitado = user.sonido;
+                  console.log("User sonido: " + this.habilitado + "||User: " + user);
+                  resolve();
+                }
+
+            })
+            .catch((error)=>{"Error al traer usuario en servicio sonido: " + error})
+        }
+        else{
+          this.habilitado = true;
+          resolve();
+        }
+
+      });
+      return promesa;
   }
 
   get_soundError(){
@@ -33,9 +65,14 @@ export class SonidosProvider {
   }
 
   reproducirSonido(sound:string){
-    this.audio.src = sound;
-    this.audio.load();
-    this.audio.play();
+    this.initialize()
+      .then(()=>{
+        if(this.habilitado){
+          this.audio.src = sound;
+          this.audio.load();
+          this.audio.play();
+        }
+      })
   }
 
 }
