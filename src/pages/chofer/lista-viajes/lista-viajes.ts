4a556adcb0bee77451c7ba896b9e9c4b07248869
EzequielCaballero <ezequiel.caballero33@gmail.com>
2018-07-12 18:12:24 -0300

    Fix modulo chofer: se agrega boton de acceso a encuesta (puede completarla en otro momento el chofer) + se muestran viajes de fecha actual + redireccionamiento directo + spinner

--- src/pages/chofer/lista-viajes/lista-viajes.ts
+++ src/pages/chofer/lista-viajes/lista-viajes.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, MenuController} from 'ionic-angular';
-import { ViajeServicio } from '../../../providers/viaje-servicio/viaje-servicio';
-import { UsuarioServicioProvider } from '../../../providers/usuario-servicio/usuario-servicio';
-//import * as firebase from 'firebase/app';
-import { ChoferViajePage } from '../../index-paginas';
-import { Subscription } from 'rxjs/Subscription';
+//PAGINAS
+import { ChoferInicioPage, ChoferViajePage, ChoferEncuestaPage } from '../../index-paginas';
+//SERVICIOS
 import { UtilidadesProvider } from '../../../providers/utilidades/utilidades';
 import { AuthServicioProvider } from '../../../providers/auth-servicio/auth-servicio';
+import { UsuarioServicioProvider } from '../../../providers/usuario-servicio/usuario-servicio';
+import { VehiculosProvider } from '../../../providers/vehiculos/vehiculos';
+import { ViajeServicio } from '../../../providers/viaje-servicio/viaje-servicio';
+//OTROS
 import * as firebase from 'firebase/app';
+import { Subscription } from 'rxjs/Subscription';
 
 @IonicPage()
 @Component({
@@ -16,122 +19,163 @@ import * as firebase from 'firebase/app';
 })
 export class ListaViajesPage {
 
+  //SPINNER
+  mostrarSpinner:boolean = false;
+  //DATOS
   viajes : any;
   chofer : any;
   viajesSubsc : Subscription;
   viajeAsignado = [];
   usuarioSesion:any;
   usersSubs : Subscription;
+  vehiculosSubs: Subscription;
   viajesSubs : Subscription;
   viaje: any;
+  hayViaje:boolean = false;
+  liberoVehiculo:boolean = false;
+  fecha:string;
+  hora:string;
 
   constructor(public navCtrl: NavController,
               public viajesProv : ViajeServicio,
-              public userProv: UsuarioServicioProvider,
+              public usuarioSrv: UsuarioServicioProvider,
+              public vehiculoSrv: VehiculosProvider,
               public menu: MenuController,
               public utils: UtilidadesProvider,
               public aut : AuthServicioProvider) {
 
+    this.mostrarSpinner = true;
     this.menu.enable(true);
     this.usuarioSesion = firebase.auth().currentUser;
 
-    this.usersSubs = this.userProv.getUsers().subscribe(
-      lista => {
+  }
+
+  ionViewWillEnter(){
+    console.log("Se está ingresando en lista viajes");
+    this.inicializar();
+    this.generar_fecha();
+    this.traerViajes();
+  }
+
+  //PRIMERA VALIDACION: chofer + viaje actual
+  inicializar(){
+    this.mostrarSpinner = true;
+    this.usersSubs = this.usuarioSrv.getUsers().subscribe(
+    lista => {
+
+  //1) OBTENER CHOFER
         this.chofer = lista.filter(v => v.id_usuario == this.usuarioSesion.uid);
-        console.log('this.chofer ',this.chofer);
-
+        console.log('Chofer en lista viajes: ',this.chofer[0]);
         if(this.chofer[0].id_viaje != '' && this.chofer[0].id_viaje != null){
-
+            this.hayViaje = true;
             this.viajesSubs = this.viajesProv.getAllTrips().subscribe(
                 lista => {
+  //2) OBTENER VIAJE
                     this.viaje = lista.filter(v => v.id_viaje == this.chofer[0].id_viaje)[0];
-                    console.log('this.viaje ',this.viaje);
-                    
-                    this.viaje.id_vehiculo = this.chofer[0].id_vehiculo;
-                    this.viajesProv.modificar_viaje(this.viaje)
-                    .then(v =>{
+                    console.log('VIAJE en lista viaje: ',this.viaje);
+  //3) VALIDAR VIAJE
+                    if(this.viaje.estado != 'pendiente' && this.viaje.estado != 'cancelado' && this.viaje.estado != 'cumplido'){
+                        this.viaje.id_vehiculo = this.chofer[0].id_vehiculo;
+                        this.viajesProv.modificar_viaje(this.viaje)
+                        .then(v =>{
 
-                        this.utils.showToast('Tiene un viaje Asignado !!');
-                        this.navCtrl.push(ChoferViajePage,{viaje :this.viaje});
+                            this.utils.showToast('Tiene un viaje Asignado');
+                            this.mostrarSpinner = false;
+                            this.navCtrl.push(ChoferViajePage,{viaje :this.viaje, chofer: this.chofer[0]});
 
-                    } )
-                    .catch(error => console.log('error ',error))
-
-
-      
+                        } )
+                        .catch(error => console.log('error ',error))
+                    }
+                    else
+                      this.mostrarSpinner = false;
                 },
                 error => this.utils.showErrorToast('Atención ! ' + error.json())
             )
-        }
+        }else
+          this.mostrarSpinner = false;
       },
       error => this.utils.showErrorToast('Atención ! ' + error.json())
     )
-
-  }
-  //VERFICAR QUE TENGA UN AUTO ASIGNADO 
-  ionViewDidLoad(){
-     
-    this.traerViajes();
-     
-    //this.usuarioSesion = firebase.auth().currentUser;
-    this.traerUsuario().then( d=> {
-      this.chofer = d;
-     }).catch(e => console.log("error ",e))
-
   }
 
-  //cancelado / pendiente / tomado / en curso / cumplido
-  traerUsuario(){
-    return  this.userProv.traerUsuario(this.aut.get_userUID());
+  //GENERAR FECHA
+  generar_fecha(){
+    let promesa = new Promise((resolve, reject)=>{
+      let currentDate = new Date();
+      this.fecha = currentDate.getFullYear()+'-'+(currentDate.getMonth()<10?'0':'').toString()+(currentDate.getMonth() + 1)+'-'+(currentDate.getDate()<10?'0':'').toString()+currentDate.getDate();
+      console.log("Fecha actual: " + this.fecha);
+      this.hora = (currentDate.getHours()<10?'0':'').toString() + currentDate.getHours().toString()+':'+ (currentDate.getMinutes()<10?'0':'').toString() +currentDate.getMinutes().toString();
+      resolve();
+    });
+    return promesa;
   }
 
+  //ESTADOS DE UN VIAJE: cancelado / pendiente / tomado / en curso / cumplido
   traerViajes(){
-  	//'pendiente','estado'
-    //this.viajeAsignado = null;
+    //TRAER VIAJES de fecha actual
     this.viajesSubsc = this.viajesProv.traerViajes()
     .subscribe(
       viajes => {
-        this.viajes = viajes.filter(v => v.estado == 'pendiente');
-        this.viajeAsignado = this.viajes.filter(v => v.id_chofer == this.aut.get_userUID());
-      
+        this.viajes = viajes.filter(v => v.estado == 'pendiente' && v.fecha == this.fecha);
+
       },
       error =>  console.log("error ",error)
     )
 
-    if(this.viajeAsignado.length>0){
-
-       console.log("Viaje asignado ",this.viajeAsignado);
-       this.conViajeAsignado(this.viajeAsignado[0]);
-       this.utils.showToast('Tiene un Viaje Asignado !','success');
-    }                     
-    console.log("Viajes ",this.viajes);
-  }
-
-  async conViajeAsignado(viaje:any){
-    
-    console.log("this.chofer.id_vehiculo ",this.chofer.id_vehiculo);
-    viaje.id_vehiculo = this.chofer.id_vehiculo;
-    viaje.estado = 'tomado';
-    await this.viajesProv.modificar_viaje(viaje);
-    this.navCtrl.push(ChoferViajePage, {viaje: viaje, chofer: this.chofer});
   }
 
   async viajeSeleccionado(viaje:any){
-    
-    viaje.id_chofer = this.chofer.id_usuario;
-    viaje.id_vehiculo = this.chofer.id_vehiculo;
+
+    this.mostrarSpinner = true;
+    this.chofer[0].id_viaje = viaje.id_viaje;
+    viaje.id_chofer = this.chofer[0].id_usuario;
+    viaje.id_vehiculo = this.chofer[0].id_vehiculo;
     viaje.estado = 'tomado';
-    await this.viajesProv.modificar_viaje(viaje);
-    this.navCtrl.push(ChoferViajePage, {viaje: viaje, chofer: this.chofer});
+    console.log("VIAJE SELECCIONADO: " + JSON.stringify(viaje));
+    console.log("Viaje chofer: " + viaje.id_chofer);
+    console.log("Viaje vehiculo: " + viaje.id_vehiculo);
+    this.usuarioSrv.modificar_usuario(this.chofer[0])
+      .then(()=>{
+        this.viajesProv.modificar_viaje(viaje)
+          .then(()=>{
+            this.mostrarSpinner = false;
+            //REDIRECCIONAMIENTO DIRECTO
+          })
+      })
+
+
   }
 
- 
+  encuestaChofer() {
+    this.navCtrl.push(ChoferEncuestaPage, { vehiculo: this.chofer[0].id_vehiculo, chofer: this.chofer[0].id_usuario, desdeInicio: false });
+  }
+
+  liberarVehiculo(){
+    this.vehiculosSubs = this.vehiculoSrv.getListaVehiculos().subscribe(next => {
+      var vehiculos = next.filter(itemVehiculo => itemVehiculo.vehiculo.patente == this.chofer[0].id_vehiculo);
+      if(vehiculos.length > 0)
+      {
+        vehiculos[0].vehiculo.ocupado = false;
+        this.vehiculoSrv.updateItem(vehiculos[0].key,vehiculos[0].vehiculo);
+        this.chofer[0].id_vehiculo = '';
+        this.usuarioSrv.modificar_usuario(this.chofer[0])
+          .then(()=>{
+            this.liberoVehiculo = true;
+            this.navCtrl.setRoot(ChoferInicioPage);
+          })
+      }
+    });
+  }
+
   ionViewWillLeave(){
-    console.log("se ejecuto ionViewWillLeave");
-    this.viajesSubs.unsubscribe();
-  
+    console.log("Se fue de lista viajes");
+    this.usersSubs.unsubscribe();
+    if(this.liberoVehiculo)
+      this.vehiculosSubs.unsubscribe();
+    if(this.hayViaje)
+      this.viajesSubs.unsubscribe();
   }
 
-  
+
 
 }
