48faac0fa549ff7701dc44eaee7c3722d71f8637
EzequielCaballero <ezequiel.caballero33@gmail.com>
2018-07-12 18:20:55 -0300

    Fix en modulo supervisor: se remueve uso de alertCtrl en photoTaker page

--- src/pages/supervisor/photo-taker/photo-taker.ts
+++ src/pages/supervisor/photo-taker/photo-taker.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Camera } from '@ionic-native/camera';
 import { cameraConfig } from '../../../config/camera.config';
 import { VehiculoImagenProvider } from '../../../providers/vehiculo-imagen/vehiculo-imagen';
 import { VehiculosProvider } from '../../../providers/vehiculos/vehiculos';
+import { UtilidadesProvider } from '../../../providers/utilidades/utilidades';
 import { vehiculo } from '../../../classes/vehiculo.model';
 // import { ImageModel } from '../../models/imageModel';
 // import { User } from '@firebase/auth-types';
@@ -32,9 +33,9 @@ export class PhotoTakerPage implements OnInit {
     public navCtrl: NavController,
     public navParams: NavParams,
     private camera: Camera,
-    private alertCtrl: AlertController,
     private vehiculoImagenSrv: VehiculoImagenProvider,
-    private vehiculoSrv: VehiculosProvider
+    private vehiculoSrv: VehiculosProvider,
+    private utilidadesSrv: UtilidadesProvider
   ) {
     this.vehiculo = this.navParams.data;
     console.log(this.navParams.data);
@@ -79,28 +80,11 @@ export class PhotoTakerPage implements OnInit {
 
   /**
    * Para borrar una imagen desde el for del html
-   * @param index El indice de la foto 
+   * @param index El indice de la foto
    */
   deletePhoto(index) {
-    let confirm = this.alertCtrl.create({
-      title: '¿Está seguro que quiere elminar la imagen?',
-      message: '',
-      buttons: [
-        {
-          text: 'No',
-          handler: () => {
-            console.log('Disagree clicked');
-          }
-        }, {
-          text: 'Si',
-          handler: () => {
-            console.log('Agree clicked');
-            this.photos.splice(index, 1);
-          }
-        }
-      ]
-    });
-    confirm.present();
+    this.photos.splice(index, 1);
+    this.utilidadesSrv.showToast("Foto eliminada");
   }
 
   /**
@@ -113,9 +97,10 @@ export class PhotoTakerPage implements OnInit {
       this.vehiculoImagenSrv.subirImagenVehiculo(this.key,element).then(result => {
         this.vehiculo.fotos.push(result);
       }).catch(error => console.log(error));
-      
+
     }
     this.vehiculoSrv.updateItem(this.key,this.vehiculo);
+    this.utilidadesSrv.showToast("Foto subida");
     this.navCtrl.pop();
   }
 
