3eee1008dc5d3be13b63938591bddc1258d83098
Lucas Bonanni <lucas_eb2004@yahoo.com.ar>
2018-07-14 02:19:02 -0300

    agrego las opciones al a página de configuraciones

--- src/pages/comunes/configuraciones/configuraciones.ts
+++ src/pages/comunes/configuraciones/configuraciones.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ActionSheetController, ActionSheetOptions } from 'ionic-angular';
+import { ThemeSettingsProvider } from '../../../providers/theme-settings/theme-settings';
 
 /**
  * Generated class for the ConfiguracionesPage page.
@@ -14,12 +15,56 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'configuraciones.html',
 })
 export class ConfiguracionesPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  isCustom = false;
+  font: string = '';
+  fontSize: string = '';
+  headerColor: string = '';
+  headerFontColor: string = '';
+  itemsBackgroundColor: string = '';
+  itemsListBackgroundColor: string = '';
+  background:string = '';
+  ionSelectOptions: ActionSheetOptions = { cssClass: 'naif-theme' };
+  soundSet: string;
+  // fontSelectOptions: ActionSheetOptions;
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public themeSetting: ThemeSettingsProvider,
+    public actionSheetCtrl: ActionSheetController) {
+    this.themeSetting.getActiveTheme().subscribe(val => this.ionSelectOptions.cssClass = val.toString());
+    // this.presentActionSheet()
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ConfiguracionesPage');
+  }
+
+  setTheme(texto: string) {
+    console.log('click en el set theme');
+    this.themeSetting.setActiveTheme(texto);
+  }
+
+  onChangeConfiguration() {
+    console.log('test change select');
+    this.contruirCustom()
+  }
+
+  contruirCustom() {
+    const space = ' ';
+    let classes =
+      this.font + space +
+      this.fontSize + space +
+      this.headerColor + space +
+      this.headerFontColor + space +
+      this.itemsBackgroundColor + space +
+      this.itemsListBackgroundColor + space +
+      this.background;
+    this.themeSetting.setActiveTheme(classes);
+  }
+
+  setButtonValue(value:string){
+    this.background = value;
+    this.contruirCustom();
   }
 
 }
